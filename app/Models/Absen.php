d0249e2236f1385edbf2cbd6b120748b2763e1de
Mufiz17 <mufizihsanulhaq@gmail.com>
2025-09-02 10:27:16 +0700

    Setup tabel absen

---
+++ app/Models/Absen.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Absen extends Model
+{
+    use HasFactory;
+    protected $table = 'absens';
+    protected $fillable = [
+        'nama',
+        'divisi',
+        'keterangan'
+    ];
+}
